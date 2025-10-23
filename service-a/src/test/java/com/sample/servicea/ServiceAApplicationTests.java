b660d24022528d11d78ed67e966eac88905140fc
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-10-23 17:04:33 +0700

    init commit

---
+++ service-a/src/test/java/com/sample/servicea/ServiceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.servicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
