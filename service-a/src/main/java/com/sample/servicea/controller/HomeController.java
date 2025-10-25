cf30300524f98c1c40e67eaed3a41bda1e191d24
toannt52 <toannt52@fpt.com>
2025-10-25 22:45:10 +0700

    rename file and add health endpoint with replacing code pattern

--- service-a/src/main/java/com/sample/servicea/controller/HomeController.java
+++ service-a/src/main/java/com/sample/servicea/controller/HomeController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Home {
+public class HomeController {
 
     @GetMapping("/")
     public String index() {
         return "I am Service A, hello world!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "I am Service A, healthy!";
     }
 }
