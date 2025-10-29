960fbfcf60d13f6a2b86c249cfcc3369d955cbca
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-10-29 15:58:44 +0700

    init Spring loadbalancer prj

---
+++ cloud-loadbalancer/halo-world/src/main/java/com/sample/haloworld/HaloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.sample.haloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaloWorldApplication.class, args);
+    }
+
+}
---
+++ cloud-loadbalancer/halo-world/src/test/java/com/sample/haloworld/HaloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.haloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
